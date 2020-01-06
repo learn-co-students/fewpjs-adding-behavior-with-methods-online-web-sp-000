873f8d0384b6988e6161324a4399b233151f7b62
stabassum <sumu1993@gmail.com>
2020-01-05 20:33:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+class Cat{
+
+    constructor(name){
+        this.name = name;
+    }
+
+    function speak(name){
+        return `${name} says meow!`;
+    }
+
+}
+
+class Dog{
+
+    function speak(name){
+        return `${name} says woof!`;
+    }
+}
+
+class Bird{
+
+    let instance = "";
+
+    if( inatance === "male"){
+        return `It's me! ${name}, the parrot!`;
+    }
+    else {
+        return `${name} says squawk!`;
+    }
+
+}
+
