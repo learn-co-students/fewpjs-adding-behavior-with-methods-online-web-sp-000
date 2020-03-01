af5742fe7744c96447cc27a5e3dd6aec86a0915f
Paul Rosero <paul_rosero@yahoo.com>
2020-03-01 18:48:05 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+class Cat {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+}
