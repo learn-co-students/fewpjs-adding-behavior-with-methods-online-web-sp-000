9897d0dae122c47bd8d0a441f78d5ecb6ae8b6f4
dayofgray <graysonpberger@gmail.com>
2020-07-11 11:59:03 -0400

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Cat {
     }
 
     speak() {
-        return `${this.name} says meow`
+        return `${this.name} says meow!`
     }
 }
 
@@ -18,7 +18,7 @@ class Dog {
     }
 
     speak() {
-        return `${this.name} says woof`
+        return `${this.name} says woof!`
     }
 }
 
