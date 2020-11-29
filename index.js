836444511ab17845eee3e5597bf1781acd5c6bcc
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-29 04:53:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+class Cat {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex
+  }
+
+  speak(){
+    console.log(`${name} says meow!`)
+  }
+}
+
+class Dog {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex
+  }
+
+  speak(){
+    console.log(`${name} says woof!`)
+  }
+}ç
+
+class Bird {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex
+  }
+
+  if Bird.sex === male {
+    console.log(`It's me! ${name}, the parrot!`)
+  } else {
+    console.log(`${name} says squawk!`)
+  }
+}
