b5df5d9e4435172a648879d37c89fad6351972c7
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-31 14:45:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+class Cat {
+    constructor(name) {
+        this.name = name;
+    },
+    speak() {
+        `${this.name} says meow!`
+    }
+}
+
+class Dog {
+}
+
+class Bird {
+}
